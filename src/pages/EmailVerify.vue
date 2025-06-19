75c3968caadc28c99ddad7d7908f9110d7b7a9db
Gorani77 <sp2877@naver.com>
2025-06-19 12:26:32 +0900

    feat: 이메일 인증 페이지 수정

--- src/pages/EmailVerify.vue
+++ src/pages/EmailVerify.vue
@@ -1,7 +1,3 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
   <div class="email-verification-container">
     <div class="email-verification-inner">
@@ -16,21 +12,29 @@
       </div>
       <div class="email-desc-box">
         <div class="email-desc-main">
-        <span>메일함을 확인해 주세요.<br>
-        인증코드를 입력하셔야<br>
-        정상적으로 회원가입이 완료됩니다!</span>
+        <span>
+          메일함을 확인해 주세요.<br>
+          인증코드를 입력하셔야<br>
+          정상적으로 회원가입이 완료됩니다!
+        </span>
         </div>
       </div>
-      <form class="email-code-form">
+      <form class="email-code-form" @submit.prevent="onVerify">
         <label class="email-code-label">
           <span>인증코드</span>
         </label>
         <div class="email-code-input-wrap">
-          <input type="text" class="email-code-input" placeholder="example@email.com" />
+          <input
+              type="text"
+              class="email-code-input"
+              v-model="code"
+              placeholder="인증코드 입력"
+              maxlength="10"
+          />
         </div>
-        <div class="email-code-error">
+        <div class="email-code-error" v-if="error">
           <i class="bi bi-x-lg"></i>
-          <span>인증코드가 올바르지 않습니다</span>
+          <span>{{ error }}</span>
         </div>
         <div class="email-submit-box">
           <button type="submit" class="btn-email-submit">이메일 인증하기</button>
@@ -38,8 +42,46 @@
       </form>
     </div>
   </div>
-
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const code = ref('');
+const error = ref('');
+const router = useRouter();
+
+// 서버에서 온 인증코드가 "123456"이라고 가정
+const correctCode = "123456";
+
+const onVerify = async () => {
+  error.value = '';
+
+  // 빈 값 체크
+  if (!code.value) {
+    error.value = '인증코드를 입력해주세요.';
+    return;
+  }
+
+  // 길이 검사(필요하면 추가)
+  if (code.value.length < 6) {
+    error.value = '6자리 인증코드를 입력해주세요.';
+    return;
+  }
+
+  // 예시: 코드가 다르면 에러
+  if (code.value !== correctCode) {
+    error.value = '인증코드가 올바르지 않습니다';
+    return;
+  }
+
+  // 인증 성공 시 알림메세지
+  alert('인증이 완료되었습니다.');
+  // 성공 시 다음 페이지 이동
+  router.push('/login');
+};
+</script>
 
 <style scoped>
 
